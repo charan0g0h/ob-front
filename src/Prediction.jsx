4ded045278fe8ecc61355f0a0f88e771d8ac1ca2
hari charan <kharicharan456@gmail.com>
2026-07-06 21:16:19 +0530

    fetch address updated

--- src/Prediction.jsx
+++ src/Prediction.jsx
@@ -118,7 +118,7 @@ export default function Prediction() {
     e.preventDefault();
     const numericData = convertToNumeric(formData);
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch(" https://obesity-1.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- src/SuggestionPage.jsx
+++ src/SuggestionPage.jsx
@@ -35,7 +35,7 @@ export default function SuggestionPage() {
             console.log(userData)
             try {
 
-                const response = await fetch("http://127.0.0.1:8000/suggest", {
+                const response = await fetch(" https://obesity-1.onrender.com/suggest", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
